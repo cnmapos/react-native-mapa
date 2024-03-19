7d3465365b80fa1aee21e6e898be8b16b4aebeb1
renyunhong <renyunhong6@gmail.com>
2024-03-19 09:22:39 +0800

    feat: format MapView

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,9 +1,9 @@
 import Mapbox from '@rnmapbox/maps';
 import { StyleSheet, View } from 'react-native';
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useEffect, useRef, useState } from 'react';
 import { MapContext } from '../MapContext';
 import { MapViewInterface, Projection, StyleIDs } from '../types';
-import { loadStyle } from '../config/style';
+import { loadStyle, styleFormat } from '../config/style';
 import EventEmitter from 'eventemitter3';
 
 /**
@@ -28,13 +28,11 @@ export type MapViewProps = {
 };
 
 class MapViewCls implements MapViewInterface {
-    private map?: Mapbox.MapView;
+    public map: Mapbox.MapView | null;
     private emitter: EventEmitter = new EventEmitter();
 
-    constructor(map: Mapbox.MapView | null | undefined) {
-        if (map) {
-            this.map = map;
-        }
+    constructor(map: Mapbox.MapView | null) {
+        this.map = map;
     }
     on(event: 'onCameraChanged', listener: (e: any) => void): void {
         this.emitter.on(event, listener);
@@ -57,17 +55,21 @@ class MapViewCls implements MapViewInterface {
 const MapView = (props: MapViewProps) => {
     const { children, style = StyleIDs.MapboxVector, projection = 'mercator' } = props;
     const [rnMap, setRNMap] = useState<Mapbox.MapView | null>(null);
-    const map = new MapViewCls(rnMap);
-    const styleContent = loadStyle(style);
-    let styleURL: any, styleJSON;
-    if (typeof styleContent === 'object') {
-        styleJSON = JSON.stringify(styleContent);
-    } else {
-        styleURL = styleContent;
-    }
+
+    const [customStyles, setStyle] = useState<{ styleURL: ReturnType<typeof loadStyle>; styleJSON: string }>(() => {
+        return styleFormat(style);
+    });
+
+    const mapRef = useRef<MapViewCls | null>(new MapViewCls(null));
+
     useEffect(() => {
-        rnMap && map.setMap(rnMap);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        setStyle(styleFormat(style));
+    }, [style]);
+
+    useEffect(() => {
+        if (rnMap && mapRef.current) {
+            mapRef.current.setMap(rnMap);
+        }
     }, [rnMap]);
 
     return (
@@ -75,17 +77,18 @@ const MapView = (props: MapViewProps) => {
             <View style={styles.container}>
                 <Mapbox.MapView
                     ref={(ref) => setRNMap(ref)}
-                    styleURL={styleURL}
-                    styleJSON={styleJSON}
+                    styleURL={customStyles.styleURL}
+                    styleJSON={customStyles.styleJSON}
                     projection={projection}
                     attributionEnabled={false}
                     logoEnabled={false}
                     zoomEnabled={true}
+                    compassEnabled={true}
                     scaleBarEnabled={false}
-                    onCameraChanged={(...args) => map.emit('onCameraChanged', ...args)}
+                    onCameraChanged={(...args) => mapRef.current?.emit('onCameraChanged', ...args)}
                     style={styles.map}
                 >
-                    <MapContext.Provider value={{ map }}>{children}</MapContext.Provider>
+                    <MapContext.Provider value={{ map: mapRef.current || null }}>{children}</MapContext.Provider>
                 </Mapbox.MapView>
             </View>
         </View>
