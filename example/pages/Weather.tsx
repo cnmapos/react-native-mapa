9ead28919569d4f977aac911d5d83920d0f04ea8
heavis <308975917@qq.com>
2024-04-02 01:20:30 +0800

    feat: 🎸 add example project

---
+++ example/pages/Weather.tsx
@@ -0,0 +1,26 @@
+import Mapa, {Slot} from 'react-native-mapa';
+import {SafeAreaView} from 'react-native';
+
+function PreView({navigation}: any): React.JSX.Element {
+    Mapa.setAccessToken(
+        'sk.eyJ1IjoiY25tYXBvcyIsImEiOiJjbHRqa2RqNzgwczhnMnFrOWNnZ2t5bHA3In0.WJN2DQHS9dwoKVHyfiBKYg',
+    );
+    return (
+        <SafeAreaView style={{height: '100%'}}>
+            <Mapa.MapView>
+                <Slot slot="rightTop" backgroundColor={'transparent'}>
+                    <Mapa.Camera />
+                </Slot>
+                <Slot
+                    slot="rightBottom"
+                    width={0.2}
+                    height={0.35}
+                    backgroundColor={'transparent'}>
+                    <Mapa.Weather akey={'64bdebe6239a3a398443b2af4ba6085e'} />
+                </Slot>
+            </Mapa.MapView>
+        </SafeAreaView>
+    );
+}
+
+export default PreView;
---
+++ example/routes.ts
@@ -0,0 +1,73 @@
+import BaseMap from './pages/BaseMap';
+import Weather from './pages/Weather';
+
+const RoutesConfig = [
+    {
+        title: '地图',
+        data: [
+            {
+                title: '基础地图',
+                img: require('./assets/basic.png'),
+                id: 'BaseMap',
+                component: BaseMap,
+            },
+            {
+                title: '底图组件',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: '控件',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: 'POI搜索',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: '天气',
+                img: require('./assets/basic.png'),
+                id: 'Weather',
+                component: Weather,
+            },
+        ],
+    },
+    {
+        title: '图层',
+        data: [
+            {
+                title: '栅格图层',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: '矢量图层',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: '热力图',
+                img: require('./assets/basic.png'),
+            },
+        ],
+    },
+    {
+        title: '绘制',
+        data: [
+            {
+                title: '折线绘制',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: '圆形绘制',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: '多边形绘制',
+                img: require('./assets/basic.png'),
+            },
+            {
+                title: '贝塞尔曲线绘制',
+                img: require('./assets/basic.png'),
+            },
+        ],
+    },
+];
+export default RoutesConfig;
