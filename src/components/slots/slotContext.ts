0a029ebe345b29397efba6238d18a8f37ab36353
renyunhong <renyunhong6@gmail.com>
2024-04-01 10:20:11 +0800

    feat: slot component add list miantain

--- src/components/slots/slotContext.ts
+++ src/components/slots/slotContext.ts
@@ -1,5 +1,6 @@
 import { createContext, ReactNode } from 'react';
-import { SlotItemType, SlotTypeEnum } from './type';
+import { ParamsType, SlotItemType, SlotTypeEnum } from './type';
+import { generateUUID } from './common';
 
 type ContextType = {
     [k in SlotTypeEnum]: SlotItemType
@@ -11,6 +12,7 @@ export const defaultSlotConfig: ContextType = {
             left: 4,
             bottom: 4,
         },
+        componentList: [],
         child: [],
         visible: false,
     },
@@ -20,6 +22,7 @@ export const defaultSlotConfig: ContextType = {
             bottom: 4,
         },
         visible: false,
+        componentList: [],
         child: []
     },
     [SlotTypeEnum.rightTop]: {
@@ -28,6 +31,7 @@ export const defaultSlotConfig: ContextType = {
             right: 4,
         },
         visible: false,
+        componentList: [],
         child: []
     },
     [SlotTypeEnum.leftTop]: {
@@ -36,6 +40,7 @@ export const defaultSlotConfig: ContextType = {
             top: 4,
         },
         visible: false,
+        componentList: [],
         child: []
     },
     [SlotTypeEnum.bottomCenter]: {
@@ -44,6 +49,7 @@ export const defaultSlotConfig: ContextType = {
             top: 0,
         },
         visible: false,
+        componentList: [],
         child: []
     },
     [SlotTypeEnum.topCenter]: {
@@ -52,6 +58,7 @@ export const defaultSlotConfig: ContextType = {
             top: 0,
         },
         visible: false,
+        componentList: [],
         child: []
     }
 };
@@ -91,12 +98,58 @@ export enum SlotActionEnum {
 
 // TODO
 export function slotReducer(store: ContextType, action: {
-    type: SlotActionEnum, payload: { [k: string]: any }
+    type: SlotActionEnum, payload: Partial<ParamsType>
 }) {
 
     const { type, payload } = action;
     switch (type) {
         case SlotActionEnum.registerSlot:
+
+            /**
+             * 1、检查当前插槽上是否存在
+             * 2、
+             */
+            const { site, key, children, options } = payload;
+            const slot = site && store[site];
+            const componentList = slot?.componentList;
+
+            if (componentList) {
+                const isExist = !!componentList.find((item) => {
+                    return item.key === key;
+                });
+
+                if (!isExist) {
+                    // 默认排队在尾部
+                    const injectSite = options?.start ? 'start' : 'end';
+                    const injectInfo = {
+                        key: key ? key : generateUUID(),
+                        visible: true,
+                        children: children
+                    };
+
+                    // 修改默认插槽渲染条件
+                    if (!slot.visible && !slot.componentList.length) {
+                        slot.visible = true;
+                    }
+
+                    // 组件插入插槽中
+                    // 目前仅支持从头插入、从尾部插入，不支持顺序设定！
+                    if (injectSite === 'start') {
+                        componentList.unshift(injectInfo);
+                    } else {
+                        componentList.push(injectInfo);
+                    }
+
+                    // 更新render child
+                    const renderList: ReactNode[] = [];
+                    componentList.forEach((item) => {
+                        renderList.push(item.children);
+                    });
+
+                    slot.child = renderList;
+                }
+            }
+
             break;
         case SlotActionEnum.changeComponentVisible:
             break;
