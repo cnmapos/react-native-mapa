7dc17902198b6a988cb793eb06f93b057ab81f03
renyunhong <renyunhong6@gmail.com>
2024-04-01 10:21:09 +0800

    feat: register slot component

---
+++ src/components/slots/SlotComp.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode, useContext, useEffect } from 'react';
+import SlotContext from './slotContext';
+import { SlotTypeEnum } from './type';
+import { generateUUID } from './common';
+
+//
+
+type SlotCompPropsType = {
+    slot: SlotTypeEnum;
+    children: ReactNode;
+    insertSite: 'start' | 'end';
+    visible?: boolean;
+    key?: string;
+};
+
+const SlotComp = ({ slot, key, visible, children, insertSite }: SlotCompPropsType) => {
+    const ctx = useContext(SlotContext);
+
+    const uuid = key ? key : generateUUID();
+
+    // 不在插槽作用域下
+    if (!ctx) {
+        throw new Error('use Slot Component must in MapView scope！');
+    }
+
+    // 不存在 children
+    if (!children) {
+        throw new Error('Slot need children node(type is ReactNode)');
+    }
+
+    // 处理唤起
+    useEffect(() => {
+        ctx.changeComponentVisible(slot, uuid, visible);
+    }, [visible]);
+
+    useEffect(() => {
+        ctx.registerSlot(slot, uuid, children, {
+            start: insertSite === 'start' ? true : undefined,
+        });
+    }, [slot]);
+
+    // Since the component is not rendering anything directly
+    return null;
+};
+
+export default SlotComp;
