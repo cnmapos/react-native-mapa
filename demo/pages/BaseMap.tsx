151e5115c582d20627cb1a2fa42511bdeb00583a
RonRayvon <renyunhong6@gmail.com>
2024-04-03 21:09:09 +0800

    feat: test demo

--- demo/pages/BaseMap.tsx
+++ demo/pages/BaseMap.tsx
@@ -1,19 +1,27 @@
 import Mapa, { Slot } from '../../src/';
 import { SafeAreaView } from 'react-native';
+import ParentComponent from '../../src/components/slots/ParentComponent';
+import SlotComp from '../../src/components/slots/SlotComp';
+import { SlotTypeEnum } from '../../src/components/slots/type';
 
 function PreView({ navigation }: any): React.JSX.Element {
     Mapa.setAccessToken('sk.eyJ1IjoiY25tYXBvcyIsImEiOiJjbHRqa2RqNzgwczhnMnFrOWNnZ2t5bHA3In0.WJN2DQHS9dwoKVHyfiBKYg');
     return (
         <SafeAreaView style={{ height: '100%' }}>
             <Mapa.MapView>
-                <Slot slot="rightTop" backgroundColor={'transparent'}>
+                <ParentComponent>
+                    <SlotComp slot={SlotTypeEnum.leftBottom} key="xxx-Camera" insertSite={'start'}>
+                        <Mapa.Camera />
+                    </SlotComp>
+                </ParentComponent>
+                {/* <Slot slot="rightTop" backgroundColor={'transparent'}>
                     <Mapa.Camera />
                 </Slot>
                 <Slot slot="rightBottom" width={0.2} height={0.35} backgroundColor={'transparent'}>
                     <Mapa.ZoomInOut />
                     <Mapa.Location style={'right-bottom'} locateWhenInit={true} visible={true} />
                 </Slot>
-                <Mapa.POIFinder placeholder={'请输入地址'} akey={'64bdebe6239a3a398443b2af4ba6085e'} />
+                <Mapa.POIFinder placeholder={'请输入地址'} akey={'64bdebe6239a3a398443b2af4ba6085e'} /> */}
             </Mapa.MapView>
         </SafeAreaView>
     );
