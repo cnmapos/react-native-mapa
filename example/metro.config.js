9d46c60bd1f7af9720b3247ee1dfd849df0b7139
heavis <308975917@qq.com>
2024-04-16 23:40:27 +0800

    feat: 🎸 format metro code

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 
@@ -9,8 +9,8 @@ const root = path.resolve(__dirname, '..');
 const libPackageJson = require('../package.json');
 
 const libPeerDependencies = Object.keys(libPackageJson.peerDependencies)
-  .concat(['@babel/runtime'])
-  .concat(Object.keys(libPackageJson.dependencies));
+    .concat(['@babel/runtime'])
+    .concat(Object.keys(libPackageJson.dependencies));
 
 const defaultConfig = getDefaultConfig(__dirname);
 
@@ -21,33 +21,35 @@ const defaultConfig = getDefaultConfig(__dirname);
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  projectRoot: __dirname,
-  watchFolders: [root],
+    projectRoot: __dirname,
+    watchFolders: [root],
 
-  resolver: {
-    blacklistRE: exclusionList(
-      libPeerDependencies.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-      ),
-    ),
+    resolver: {
+        blacklistRE: exclusionList(
+            libPeerDependencies.map(
+                m =>
+                    new RegExp(
+                        `^${escape(path.join(root, 'node_modules', m))}\\/.*$`,
+                    ),
+            ),
+        ),
 
-    extraNodeModules: libPeerDependencies.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
+        extraNodeModules: libPeerDependencies.reduce((acc, name) => {
+            acc[name] = path.join(__dirname, 'node_modules', name);
+            return acc;
+        }, {}),
 
-    assetExts: [...defaultConfig.resolver.assetExts, 'gltf', 'glb'],
-  },
+        assetExts: [...defaultConfig.resolver.assetExts, 'gltf', 'glb'],
+    },
 
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
 };
 
 module.exports = mergeConfig(defaultConfig, config);
