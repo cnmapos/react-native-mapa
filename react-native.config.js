5b868ddefb892732246a605616cc79a9caeec2bc
heavis <308975917@qq.com>
2024-06-24 22:32:12 +0800

    feat: 🎸 add pod link in podfile

--- react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    dependencies: {
-        '@rnmapbox/maps': {
-            platforms: {
-                ios: null, // This ensures the iOS platform is included
-            },
-        },
-    },
-};
